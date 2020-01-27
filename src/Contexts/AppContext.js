7df9772fb7d1300f5ba9f4ffbdace41e696de0de
briecodes <hello@brie.work>
2020-01-27 11:54:40 -0500

    Removed unused context.

--- src/Contexts/AppContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const AppContext = React.createContext({score: 0, health: 100, gameStage: 'start'});
-export const AppProvider = AppContext.Provider;
-export const AppConsumer = AppContext.Consumer;
-export const AppGame = AppContext.Game;
-
-export default AppContext;
